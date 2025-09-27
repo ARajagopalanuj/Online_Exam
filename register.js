95bd525387c05c6119f4a90419e0f2448a1c53fe
Rajagopalan A <a.rajagopalan415@gmail.com>
2025-09-27 09:16:58 +0530

    api update

--- register.js
+++ register.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const user = document.getElementById("reg-username").value;
 
         try {
-            const response = await fetch(" https://478e9b713e32.ngrok-free.app/exam/user/register", {
+            const response = await fetch("  https://9aa6e279a946.ngrok-free.app/exam/user/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ user })
@@ -46,7 +46,7 @@ if(loginForm){
                 user:user,
                 password:password
             }
-            const response= await fetch("https://478e9b713e32.ngrok-free.app/exam/user/login",{
+            const response= await fetch(" https://9aa6e279a946.ngrok-free.app/exam/user/login",{
                 method:"POST",
                 headers:{"Content-Type":"application/json"},
                 body:JSON.stringify(userDetails)
@@ -89,7 +89,7 @@ if(loginForm){
         button.textContent="AArambikalaama";
 
         button.addEventListener("click",async ()=>{
-            const apiUrl=`https://478e9b713e32.ngrok-free.app/exam/user/${element.topic}`;
+            const apiUrl=` https://9aa6e279a946.ngrok-free.app/exam/user/${element.topic}`;
             try{
                 const response=await fetch(apiUrl,{
                     method:"GET",
